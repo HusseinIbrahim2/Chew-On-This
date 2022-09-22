4b4ab82954ecf79375c601d2b53d5a510f5baea8
DarkLord239 <husseinib23920@gmail.com>
2022-09-22 14:22:18 +0300

    Using Redux state in FavoritesScreen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,16 +1,30 @@
-import { useContext } from "react";
+//import { useContext } from "react";
 import { Text, StyleSheet, View } from "react-native";
+import { useSelector } from "react-redux";
 
 import MealsList from "../components/MealsList/MealsList";
-import { FavoritesContext } from "../store/context/favorites-context";
+//import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/data";
 
 function FavoritesScreen() {
-    const favoriteMealsCtx = useContext(FavoritesContext);
 
+    /*const favoriteMealsCtx = useContext(FavoritesContext);
     const favoriteMeals = MEALS.filter(
         (meal) => favoriteMealsCtx.ids.includes(meal.id)
     )
+    if (favoriteMeals.length == 0) {
+        return (
+            <View style={styles.container} >
+                <Text style={styles.text} >You have no favorite Meals !</Text>
+            </View>
+        )
+    }*/
+
+    const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+
+    const favoriteMeals = MEALS.filter(
+        (meal) => favoriteMealIds.includes(meal.id)
+    );
 
     if (favoriteMeals.length == 0) {
         return (
@@ -18,17 +32,17 @@ function FavoritesScreen() {
                 <Text style={styles.text} >You have no favorite Meals !</Text>
             </View>
         )
-    }
-
-    return (
-        <MealsList items={favoriteMeals} />
-    )
+}
+        return (
+            <MealsList items={favoriteMeals} />
+        )
+    
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:0.8,
-        justifyContent:'center',
+    container: {
+        flex: 0.8,
+        justifyContent: 'center',
     },
     text: {
         textAlign: 'center',
